a7cfe2e902311368aeab9d114951dcfd3e7bf289
ReadyToDevNow <learntolearn0@gmail.com>
2025-03-12 16:25:26 +0100

    feat(): chart js graphique

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -1,9 +1,91 @@
 <template>
-  <div class="bg-green-500 left-72 top-16 absolute inset-0">
-    <div class="bg-blue-500 font-bold text-white text-2xl m-12 absolute inset-0">Main Content</div>
+  <div class="left-72 top-16 absolute inset-0">
+    <div class="font-bold text-white text-2xl m-12 absolute inset-0">
+      <div>Main Content</div>
+      <div>
+        <canvas id="myChart"></canvas>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script>
+import { onMounted } from 'vue'
+import { Chart, registerables } from 'chart.js'
+
+Chart.register(...registerables)
+
+export default {
+  setup() {
+    const chartData = {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    }
+
+    const chartOptions = {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    }
+
+    onMounted(() => {
+      const ctx = document.getElementById('myChart').getContext('2d')
+      const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: chartData,
+        options: chartOptions,
+      })
+      return myChart
+    })
+  },
+}
+
+/* const ctx = document.getElementById('myChart')
+
+const myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+})*/
+</script>
 
 <style></style>
