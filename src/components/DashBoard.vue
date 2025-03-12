812ae9c0bc6eee8b9a96563c076564a0f4f5dce8
ReadyToDevNow <learntolearn0@gmail.com>
2025-03-12 16:00:52 +0100

    feat: structuring application

---
+++ src/components/DashBoard.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-green-500 left-72 top-16 absolute inset-0">
+    <div class="bg-blue-500 font-bold text-white text-2xl m-12 absolute inset-0">Main Content</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,17 +2,20 @@
   <div class="dashboard">
     <SideBar />
     <HeadBar />
+    <DashBoard />
   </div>
 </template>
 
 <script>
 import SideBar from '@/components/SideBar.vue'
 import HeadBar from '@/components/HeadBar.vue'
+import DashBoard from '@/components/DashBoard.vue'
 
 export default {
   components: {
     SideBar,
     HeadBar,
+    DashBoard,
   },
 }
 </script>
