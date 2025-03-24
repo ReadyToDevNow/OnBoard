0ab4e80a37765f2e654f2d971baa1a2625b5245b
ReadyToDevNow <learntolearn0@gmail.com>
2025-03-24 17:06:38 +0100

    feat(router): Dashboard views

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,10 +1,10 @@
 <script setup></script>
 
 <template>
-  <div class="bg-blue-900 absolute inset-y-0 top-16 left-0 w-72">
+  <div class="bg-black absolute inset-y-0 top-16 left-0 w-72">
     <div class="flex flex-col justify-center items-center font-bold text-white mt-4">
       <div>Accueil</div>
-      <div>Dashboard</div>
+      <router-link to="about">Dashboard</router-link>
       <router-link to="analytics">Analytics</router-link>
       <router-link to="discover">Discover</router-link>
       <router-link to="sales">Sales</router-link>
