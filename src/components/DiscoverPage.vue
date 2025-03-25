f901248225719c6a2b60626695026bda61e45536
ReadyToDevNow <learntolearn0@gmail.com>
2025-03-26 00:25:24 +0100

    feat(design): Grid template

--- src/components/DiscoverPage.vue
+++ src/components/DiscoverPage.vue
@@ -12,12 +12,13 @@
             <span>Developer / VueJs</span>
           </div>
         </div>
-        <!-- Remplacer par une boucle for pour gérer l'affichage de plusieurs photo de profils et des noms d'utilisateurs-->
+
         <div class="flex flex-nowrap p-8 items-center overflow-x-scroll">
           <div class="flex flex-col items-center">
             <img class="h-20 w-20 mr-4" src="../assets/logo-add.png" />
             <span>Ajouter</span>
           </div>
+          <!-- Remplacer par une boucle for pour gérer l'affichage de plusieurs photo de profils et des noms d'utilisateurs via-->
           <div class="flex flex-row">
             <div class="flex flex-col items-center">
               <img class="h-20 w-20 rounded-full object-cover mr-4" src="../assets/bg-temple.png" />
@@ -57,9 +58,49 @@
             </div>
           </div>
         </div>
+        <!-- 2 eme Partie -->
+        <div class="mt-16 grid grid-cols-12">
+          <div class="col-start-1 col-end-8 col-span-3 border">
+            <div clas="flex flex-col items-center">
+              <span> Harry Potter </span>
+              <span>Il y a 3 jours</span>
+            </div>
+          </div>
 
-        <span class="flex justify-center font-bold border">Discover</span>
-        <span class="flex justify-center font-bold border">Discover</span>
+          <span class="flex col-start-8 col-end-13 justify-center font-bold border">
+            Digital Marketing
+          </span>
+        </div>
+
+        <span class="flex justify-center font-bold">Le reste</span>
+        <span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span
+        ><span class="flex justify-center font-bold">Le reste</span>
       </div>
     </div>
   </div>
