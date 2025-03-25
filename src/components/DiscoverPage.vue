fca953ffdf3a73f67a22f9a05671f71e6bee6f06
ReadyToDevNow <learntolearn0@gmail.com>
2025-03-25 16:51:30 +0100

    feat(design): background for discover

--- src/components/DiscoverPage.vue
+++ src/components/DiscoverPage.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="left-78 top-18 right-8 absolute z-49">
+    <img class="h-80 w-300" src="../assets/bg-city.jpg" alt="" />
     <div class="grid grid-cols-12 gap-4 border">
       <span class="font-bold">Discover Page </span>
       <span class="font-bold">Discover Page </span>
@@ -13,93 +14,8 @@
       <span class="font-bold">Discover Page </span>
       <span class="font-bold">Discover Page </span>
       <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span><span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
-      <span class="font-bold">Discover Page </span>
     </div>
+    <div></div>
   </div>
 </template>
 
