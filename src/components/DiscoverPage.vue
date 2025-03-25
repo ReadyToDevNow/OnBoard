75887093fc79f574a724e7aec0f691b70b0c9d01
ReadyToDevNow <learntolearn0@gmail.com>
2025-03-25 18:43:45 +0100

    feat(design): rounded pic

--- src/components/DiscoverPage.vue
+++ src/components/DiscoverPage.vue
@@ -6,14 +6,21 @@
     <div class="grid grid-cols-12 gap-4 border">
       <div class="col-span-10 col-start-2 border">
         <div class="flex items-center border">
-          <img class="h-10" src="../assets/bg-temple.png" />
-          <span>Ragnar Lothbrok</span>
-          <span>VueJs Developer</span>
+          <img class="h-30 w-30 rounded-full object-cover mr-4" src="../assets/bg-temple.png" />
+          <div class="flex flex-col items-center">
+            <span class="font-bold">Ragnar Lothbrok</span>
+            <span>Developer / VueJs</span>
+          </div>
+        </div>
+        <div class="flex p-4 mr-8 items-center overflow-x-auto">
+          <span class="flex font-bold border">pic </span>
+          <span class="flex font-bold border">pic </span>
+          <span class="flex font-bold border">pic </span>
+          <span class="flex font-bold border">pic </span>
         </div>
 
-        <span class="flex justify-center font-bold border">Discover </span>
-        <span class="flex justify-center font-bold border">Discover </span>
-        <span class="flex justify-center font-bold border">Discover </span>
+        <span class="flex justify-center font-bold border">Discover</span>
+        <span class="flex justify-center font-bold border">Discover</span>
       </div>
     </div>
   </div>
