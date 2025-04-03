336e374430947d29f1fe90c5bbf113f637549780
ReadyToDevNow <learntolearn0@gmail.com>
2025-04-03 21:12:04 +0200

    feat: Structuring page

--- src/components/AnalyticsPage.vue
+++ src/components/AnalyticsPage.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>CreativeOne</div>
+  <div class="left-78 top-8 right-8 absolute z-49">
+    <div class="grid grid-cols-3 gap-4 mb-4">
+      <div class="border">Card 1</div>
+      <div class="border">Card 2</div>
+      <div class="border">Card 2</div>
+    </div>
+    <div class="grid grid-cols-6 gap-4 mb-4">
+      <div class="col-span-2 border">Card 1</div>
+      <div class="col-span-4 border">Card 2</div>
+    </div>
+    <div class="grid grid-cols-6 gap-4 mb-4">
+      <div class="col-span-4 border">Card 1</div>
+      <div class="col-span-2 border">Card 2</div>
+    </div>
+    <div class="border">Top SellingProducts</div>
+  </div>
 </template>
 
 <script setup></script>
