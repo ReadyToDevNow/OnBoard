e0c3063ff6a54f04cd18dfe8df15b52d45812c67
ReadyToDevNow <learntolearn0@gmail.com>
2025-04-19 18:41:28 +0200

    feat(design): background sizing and pricing

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -28,7 +28,7 @@
         <img
           src="../assets/bg-purple.jpg"
           alt="item.name"
-          class="w-full md:w-1/3 h-48 object-cover"
+          class="w-full md:w-2/3 h-48 object-cover"
         />
         <div class="p-4 flex flex-col justify-between">
           <div>
@@ -55,21 +55,28 @@ const items = [
     id: 1,
     name: 'Pack de sprites Fantasy',
     description: 'Un set de personnages et monstres pixel art.',
-    price: 15,
+    price: 150.99,
     image: '/vecteezy_abstract-blue-and-purple-dynamic-background-modern-gradient_48061477.jpg',
   },
   {
     id: 2,
     name: 'Icônes UI modernes',
     description: 'Icônes vectorielles parfaites pour vos jeux ou apps.',
-    price: 9,
+    price: 90.99,
     image: 'https://via.placeholder.com/300x200?text=UI+Icons',
   },
   {
     id: 3,
     name: 'Arrière-plans de villes futuristes',
     description: 'Backgrounds en haute qualité pour side-scrollers.',
-    price: 19,
+    price: 190.99,
+    image: 'https://via.placeholder.com/300x200?text=Backgrounds',
+  },
+  {
+    id: 4,
+    name: 'All For One',
+    description: 'Le kit ultime',
+    price: 399.99,
     image: 'https://via.placeholder.com/300x200?text=Backgrounds',
   },
 ]
